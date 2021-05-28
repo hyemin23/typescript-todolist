69f7e2176d9da8db4943df56c22a35d51b4960a0
hyemin23 <hm_stom@naver.com>
2021-05-29 01:10:55 +0900

    #
    #

---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { NextPage } from "next";
+import TodoList from "../components/TodoList";
+import { getTodosAPI } from "../lib/api/todo";
+import { wrapper } from "../store";
+import { todoActions } from "../store/todo";
+
+const app: NextPage = () => {
+  return <TodoList />;
+};
+export const getServerSideProps = wrapper.getServerSideProps(
+  async ({ store }) => {
+    try {
+      const { data } = await getTodosAPI();
+      store.dispatch(todoActions.setTodo(data));
+      return { props: {} };
+    } catch (e) {
+      console.log(e);
+      return { props: {} };
+    }
+  }
+);
+
+export default app;
